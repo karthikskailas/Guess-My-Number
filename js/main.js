7662cdf44bf2115873987c3c61f380346fc45459
Karthik s Kailas <karthikskailas@gmail.com>
2022-08-18 15:19:55 +0530

    fix submit error  and add  innertext  and set timeout .

--- js/main.js
+++ js/main.js
@@ -1,25 +1,54 @@
 let userId;
+const mainContentForm = document.querySelector("#maincontent__form"); // Selecting form from maincontent
+const userInput = document.querySelector("#userId"); // Selecting input inside the form
+const mainPageDisplayBox = document.querySelector(
+	"#mainpage__displaybox-content"
+);
+const mainPageInputBox = document.querySelector("#mainpage__inp");
+
 /* ---------- Set Data and Get data to session Storage --------- */
 
 const setSessionStorage = () => {
 	sessionStorage.setItem("userName", userId);
 };
 const getSessionStorage = () => {
-	sessionStorage.getItem("userName");
+	userId = sessionStorage.getItem("userName");
+};
+
+const textAdd = () => {
+	const mainPageTxt = document.querySelector("#mainpage__txt");
+
+	if (mainPageTxt) {
+		mainPageTxt.innerText = `Welcome, ${userId}`;
+		setTimeout(() => {
+			mainPageTxt.innerText = `start guessing`;
+		}, 1500);
+	}
 };
 /* ------------------ Accessing user Input -------------------- */
-
-const mainContentForm = document.querySelector("#maincontent__form"); // Selecting form from maincontent
-const userInput = document.querySelector("#userId"); // Selecting input inside the form
-
 //to access the user's name inside the input and added to the session storage
 
 const userNames = () => {
-	userId = userInput.value;
-	prompt(userId);
+	userId = userInput.value; //accessing input value
 	setSessionStorage();
 };
 
-// to trigger the function when it is submitted
+//to check if mainContentForm exist or not
 
-mainContentForm.addEventListener("submit", userNames);
+if (mainContentForm) {
+	// to trigger the function when it is submitted in the homepage
+	mainContentForm.addEventListener("submit", userNames);
+}
+
+getSessionStorage();
+textAdd();
+
+/* ----- for Displaying input number in the display box */
+
+mainPageInputBox.addEventListener("input", () => {
+	if (mainPageInputBox.value === "") {
+		mainPageDisplayBox.innerText = "?";
+	} else {
+		mainPageDisplayBox.innerText = mainPageInputBox.value;
+	}
+});
